68da921269fd6ccb0377534a79fa52c8a657a29e
Jacob <jacobjohnschnell@gmail.com>
2020-05-04 15:26:41 -0400

    Uploaded solution to project Euler 23

---
+++ Individual problems/Project Euler 23.py
@@ -0,0 +1,47 @@
+from time import time
+
+
+def is_abundant(n):
+    from math import sqrt, floor
+    div_sum = 1
+    for i in range(2, floor(sqrt(n)) + 1):
+        if n % i == 0:
+            div_sum += i + (n / i if i * i != n else 0)
+    if div_sum > n:
+        return True
+    return False
+
+
+abundants = []
+upto = 28123
+for i in range(1, upto):
+    if is_abundant(i):
+        abundants.append(i)
+
+
+def worker_function(num):
+    global abundants
+
+    def is_summed_abundant(num):
+        for i in abundants:
+            if i > num / 2:
+                return False
+            elif num - i in abundants:
+                return True
+        return False
+
+    if num < 24 or not is_summed_abundant(num):
+        return num
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    start = time()
+
+    from multiprocessing import Pool
+    with Pool(30) as p:
+        unsummables = p.map(worker_function, range(1, upto + 1))
+    print(sum(unsummables))
+
+    print('Completed in', round((time() - start) * 1000), 'ms')
