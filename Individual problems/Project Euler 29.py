2385a2264a7cf5c981acd2d428d66d65d8d29574
Jacob <jacobjohnschnell@gmail.com>
2020-05-05 23:10:51 -0400

    Removed debugging smoke test

--- Individual problems/Project Euler 29.py
+++ Individual problems/Project Euler 29.py
@@ -6,10 +6,9 @@ def powers_count(a_upto, b_upto):
     for a in range(2, a_upto + 1):
         for b in range(2, b_upto + 1):
             powers.add(pow(a, b))
-    print(sorted(powers))
     return len(powers)
 
 
 start = time()
-print(powers_count(6, 6))
+print(powers_count(100, 100))
 print('Completed in', round((time() - start) * 1000), 'ms')
